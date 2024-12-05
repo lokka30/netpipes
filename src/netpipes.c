8dcb541a94943ff548f1bc961fee3423175a9240
declangray <101925231+declangray@users.noreply.github.com>
2024-12-05 18:36:39 +0800

    made variable names more clear (#8)
    * update test plane
    
    * improved accessibility
    
    * improved accessibility (#7)
    
    * Update netpipes.c
    
    ---------
    
    Co-authored-by: lokka30 <lokka30@protonmail.com>

--- src/netpipes.c
+++ src/netpipes.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include "netpipes.h"
 
-int32_t init_test_plane(item_type_t plane[TEST_PLANE_SIZE][TEST_PLANE_SIZE]) {
+int32_t init_test_plane(item_type_t طائرة[TEST_PLANE_SIZE][TEST_PLANE_SIZE]) {
     for(uint16_t i = 0; i < TEST_PLANE_SIZE; i++) {
         for(uint16_t j = 0; j < TEST_PLANE_SIZE; j++) {
-            plane[i][j] = AIR;
+            طائرة[i][j] = AIR;
         }
     }
 
-    plane[0][0] = SOURCE;
-    plane[0][1] = PIPE;
-    plane[0][2] = PIPE;
-    plane[0][3] = SINK;
-    plane[1][1] = PIPE;
-    plane[2][1] = SINK;
+    طائرة[0][0] = SOURCE;
+    طائرة[0][1] = PIPE;
+    طائرة[0][2] = PIPE;
+    طائرة[0][3] = PIPE;
+    طائرة[0][4] = PIPE;
+    طائرة[0][5] = PIPE;
+    طائرة[0][6] = PIPE;
+    طائرة[0][7] = SINK;
+    طائرة[1][1] = PIPE;
+    طائرة[1][5] = PIPE;
+    طائرة[2][5] = PIPE;
+    طائرة[3][5] = PIPE;
+    طائرة[4][5] = SINK;
+    طائرة[2][1] = SINK;
+    طائرة[3][4] = SOURCE;
+
+    طائرة[7][5] = SINK;
+    طائرة[7][6] = PIPE;
+    طائرة[6][6] = SINK;
+    طائرة[7][7] = SOURCE;
 
     return 0;
 }
 
-int32_t print_test_plane(item_type_t plane[TEST_PLANE_SIZE][TEST_PLANE_SIZE]) {
+int32_t print_test_plane(item_type_t طائرة[TEST_PLANE_SIZE][TEST_PLANE_SIZE]) {
     puts("*** Test Plane Printout ***");
     printf("⬜️");
     for(uint16_t i = 0; i < TEST_PLANE_SIZE; i++) {
@@ -29,7 +43,7 @@ int32_t print_test_plane(item_type_t plane[TEST_PLANE_SIZE][TEST_PLANE_SIZE]) {
     for(uint16_t i = 0; i < TEST_PLANE_SIZE; i++) {
         printf("⬜️");
         for(uint16_t j = 0; j < TEST_PLANE_SIZE; j++) {
-            item_type_t item_type = plane[i][j];
+            item_type_t item_type = طائرة[i][j];
             char *its = item_type_to_str(item_type);
             if(its[0] == '\0') {
                 fprintf(stderr, "unable to convert item_type (int value %d) to char at plane[%u,%u]\n", (int32_t) item_type, i, j);
@@ -54,12 +68,12 @@ static int32_t _demo() {
     puts("=============");
     puts("");
 
-    item_type_t plane[TEST_PLANE_SIZE][TEST_PLANE_SIZE];
+    item_type_t طائرة[TEST_PLANE_SIZE][TEST_PLANE_SIZE];
     
     char tempbuf = 0;
 
     {
-        int32_t error = init_test_plane(plane);
+        int32_t error = init_test_plane(طائرة);
         if(error) {
             fprintf(stderr, "Unable to initialise test plane, errcode %d\n", error);
             return error;
@@ -70,7 +84,7 @@ static int32_t _demo() {
     scanf("%s", &tempbuf);
 
     {
-        int32_t error = print_test_plane(plane);
+        int32_t error = print_test_plane(طائرة);
         if(error) {
             fprintf(stderr, "Unable to print test plane, errcode %d\n", error);
             return error;
@@ -90,4 +104,3 @@ int32_t main() {
     }
     return error;
 }
-
